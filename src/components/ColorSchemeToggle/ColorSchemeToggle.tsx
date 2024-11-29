23b5545517e0b3e6ad990a6c7e5cfd90c2bcb386
barbajoe <sabe12070@gmail.com>
2024-11-29 10:17:37 -0600

    touchup

--- src/components/ColorSchemeToggle/ColorSchemeToggle.tsx
+++ src/components/ColorSchemeToggle/ColorSchemeToggle.tsx
@@ -1,13 +1,19 @@
-import { Button, Group, useMantineColorScheme } from "@mantine/core";
+import { Button, Group, Text, useMantineColorScheme } from "@mantine/core";
+
+import "./ColorSchemeToggle.css";
 
 export function ColorSchemeToggle() {
   const { setColorScheme } = useMantineColorScheme();
 
   return (
-    <Group justify="center" mt="xl">
-      <Button onClick={() => setColorScheme("light")}>Light</Button>
-      <Button onClick={() => setColorScheme("dark")}>Dark</Button>
-      <Button onClick={() => setColorScheme("auto")}>Auto</Button>
-    </Group>
+    <div className="color-scheme-container">
+      <Text>To adjust the Color Scheme, select from the options below.</Text>
+
+      <Group mt="xl">
+        <Button onClick={() => setColorScheme("light")}>Light</Button>
+        <Button onClick={() => setColorScheme("dark")}>Dark</Button>
+        <Button onClick={() => setColorScheme("auto")}>Auto</Button>
+      </Group>
+    </div>
   );
 }
--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,27 +1,32 @@
 import { useContext } from "react";
 import { GlobalContext } from "src/GlobalContext";
 import { Anchor, Text, Title } from "@mantine/core";
-import classes from "./Welcome.module.css";
+
+import "./Welcome.css";
 
 export const Welcome = () => {
   const { name } = useContext(GlobalContext);
 
   return (
     <>
-      <Title className={classes.title} ta="center" mt={100}>
+      <Title className="title" ta="center" mt={100}>
         Welcome to{" "}
         <Text inherit variant="gradient" component="span" gradient={{ from: "pink", to: "yellow" }}>
           {name}
         </Text>
       </Title>
 
-      <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
+      <Text ta="center" size="lg" maw={580} mx="auto" mt="xl">
         This starter Vite project includes a minimal setup, if you want to learn more on Mantine +
         Vite integration follow{" "}
         <Anchor href="https://mantine.dev/guides/vite/" size="lg">
           this guide
         </Anchor>
         . To get started edit pages/Home.page.tsx file.
+      </Text>
+
+      <Text ta="center" className="margin-top">
+        To learn more about this project visit <Anchor href="/about">About</Anchor>
       </Text>
     </>
   );
